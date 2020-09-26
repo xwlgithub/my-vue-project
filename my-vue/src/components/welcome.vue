878a9f2a1f935c215a293bec8916772e92684280
xwlgithub <2509647976@qq.com>
2020-09-26 17:40:46 +0800

    首页轮播图设计完成

--- my-vue/src/components/welcome.vue
+++ my-vue/src/components/welcome.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
-    首页
+    <el-carousel :interval="4000" type="card"   height="300px">
+      <el-carousel-item v-for="item in imageList" :key="item.id">
+        <img style="height: 300px" :src="item.idView" class="image">
+      </el-carousel-item>
+    </el-carousel>
+    <div>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
+  import one from  '../images/1.jpg'
+  import two from  '../images/2.jpg'
+  import three from  '../images/3.jpg'
+  import four from  '../images/4.jpg'
+  import five from  '../images/5.jpg'
+  import six from  '../images/6.jpg'
+  import seven from  '../images/7.jpg'
+  import eight from  '../images/8.jpg'
+  import nine from  '../images/9.jpg'
     export default {
         name: "welcome",
       data() {
         return {
-
+            imageList:[
+              {id:0,idView:one},
+              {id:1,idView:two},
+              {id:2,idView:three},
+              {id:3,idView:four},
+              {id:4,idView:five},
+              {id:5,idView:six},
+              {id:6,idView:seven},
+              {id:7,idView:eight},
+              {id:8,idView:nine},
+            ]
         }
       },
       methods: {
@@ -22,5 +48,19 @@
 </script>
 
 <style scoped>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
 
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
--- my-vue/src/main.js
+++ my-vue/src/main.js
@@ -3,16 +3,15 @@ import App from './App'
 import router from  './router/index'
 import elementUi from 'element-ui'
 import mixin from './mixin'
-//import VueAxios from 'vue-axios'
+import VueAxios from 'vue-axios'
 import 'element-ui/lib/theme-chalk/index.css';
 import './css/global.css'
 Vue.config.productionTip = false
-// Vue.use(VueAxios,axios)
 Vue.use(elementUi)
 
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template:'<App/>'
 })
